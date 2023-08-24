9ae7d5875382c5c2302432064990a62135f7ceec
amos <amoschoouqconnect@gmail.com>
2023-08-24 10:41:11 +1000

    edit ireceptor trimming to include new features

--- misc/trim_airr_ireceptor_bulk.sh
+++ misc/trim_airr_ireceptor_bulk.sh
@@ -20,31 +20,30 @@ while IFS= read -r -d '' file; do
     fi
 done < <(find "$src_dir" -type f -name "*.tsv" ! -name "*_extracted.tsv" -print0)
 
-# Start trimming the TSV files
-while IFS= read -r -d '' file; do
-    outfile="${file%.tsv}_extracted.tsv"
-    awk -F '\t' -v OFS='\t' 'NR==1 {
-        for (i=1; i<=NF; i++) {
-            if ($i=="sequence_id") c1=i;
-            if ($i=="productive") c2=i;
-            if ($i=="complete_vdj") c3=i;
-            if ($i=="v_call") c4=i;
-            if ($i=="j_call") c5=i;
-            if ($i=="c_call") c10=i;
-            if ($i=="junction") c6=i;
-            if ($i=="junction_aa") c7=i;
-            if ($i=="consensus_count") c8=i;
-            if ($i=="duplicate_count") c9=i
-        }
-        seq_id=0
+awk -F '\t' -v OFS='\t' 'NR==1 {
+    for (i=1; i<=NF; i++) {
+        if ($i=="sequence_id") c1=i;
+        if ($i=="productive") c2=i;
+        if ($i=="complete_vdj") c3=i;
+        if ($i=="v_call") c4=i;
+        if ($i=="j_call") c5=i;
+        if ($i=="junction") c6=i;
+        if ($i=="junction_aa") c7=i;
+        if ($i=="consensus_count") c8=i;
+        if ($i=="duplicate_count") c9=i
     }
-    {
-        if ($c1=="" || $c1=="NULL") {
-            seq_id++;
-            $c1=seq_id
-        }
-        print $c1, $c2, $c3, $c4, $c5, $c10, $c6, $c7, $c8, $c9
-    }' "$file" > "$outfile"
+    seq_id=0
+    print "sequence_id", "productive", "complete_vdj", "v_call", "j_call", "junction", "junction_aa", "consensus_count", "duplicate_count"
+}
+NR>1 {
+    if ($c1=="" || $c1=="NULL") {
+        seq_id++;
+        $c1=seq_id
+    }
+    if (substr($c7,1,1)=="C" && (substr($c7,length($c7),1)=="F" || substr($c7,length($c7),1)=="W")) {
+        print $c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9
+    }
+}' "$file" > "$outfile"
     # Copy the output file back to its original directory
     cp "$outfile" "$(dirname "${file/#$tmp_dir\/ireceptor/$1}")/"
 done < <(find "$tmp_dir/ireceptor" -name "*.tsv" ! -name "*_extracted.tsv" -print0)
--- misc/trim_airr_ireceptor_single.sh
+++ misc/trim_airr_ireceptor_single.sh
@@ -10,7 +10,6 @@ fi
 src_file="$1"
 dest_file="$2"
 
-# Trim the TSV file
 awk -F '\t' -v OFS='\t' 'NR==1 {
     for (i=1; i<=NF; i++) {
         if ($i=="sequence_id") c1=i;
@@ -18,18 +17,20 @@ awk -F '\t' -v OFS='\t' 'NR==1 {
         if ($i=="complete_vdj") c3=i;
         if ($i=="v_call") c4=i;
         if ($i=="j_call") c5=i;
-        if ($i=="c_call") c10=i;
         if ($i=="junction") c6=i;
         if ($i=="junction_aa") c7=i;
         if ($i=="consensus_count") c8=i;
         if ($i=="duplicate_count") c9=i
     }
     seq_id=0
+    print "sequence_id", "productive", "complete_vdj", "v_call", "j_call", "junction", "junction_aa", "consensus_count", "duplicate_count"
 }
-{
+NR>1 {
     if ($c1=="" || $c1=="NULL") {
         seq_id++;
         $c1=seq_id
     }
-    print $c1, $c2, $c3, $c4, $c5, $c10, $c6, $c7, $c8, $c9
+    if (substr($c7,1,1)=="C" && (substr($c7,length($c7),1)=="F" || substr($c7,length($c7),1)=="W")) {
+        print $c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9
+    }
 }' "$src_file" > "$dest_file"
