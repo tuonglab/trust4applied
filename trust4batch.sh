f6fd3cc5d78c511b88d85c6c5f31babce77028ad
amos <amoschoouqconnect@gmail.com>
2024-03-12 12:13:44 +1000

    update trust4 scripts

--- trust4batch.sh
+++ trust4batch.sh
@@ -2,14 +2,14 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=72
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=150
+#SBATCH --mem=64G
 #SBATCH --job-name=trust4
-#SBATCH --time=96:00:00
+#SBATCH --time=336:00:00
 #SBATCH --partition=general
 #SBATCH --account=a_kelvin_tuong
-#SBATCH -o trust4run_phs002620.output
-#SBATCH -e trust4run_phs002620.error
+#SBATCH -o trust4run_phs002517.output
+#SBATCH -e trust4run_phs002517.error
 
 output_dir="$1"
 bam_files_dir="$2"
--- trust4run.sh
+++ trust4run.sh
@@ -15,15 +15,21 @@ reference_file="$3"
 mkdir -p "$output_dir"
 
 for bamfile in "$bam_files_dir"/*.bam; do
-    rsync -avz "$bamfile" "$TMPDIR/" --progress
-
     # Process bam file in $TMPDIR
     dir=$(basename "$bamfile" .bam)
     output_subdir="$output_dir/$dir"
+
+    # Skip processing if the output subdir exists and it contains files
+    if [ -d "$output_subdir" ] && [ "$(ls -A $output_subdir)" ]; then
+        echo "Output subdir $output_subdir exists and is not empty. Skipping..."
+        continue
+    fi
+
+    rsync -avz "$bamfile" "$TMPDIR/" --progress
 
     mkdir -p "$output_subdir"
     echo "Running run-trust4 for $bamfile"
 
     # Run run-trust4 on the bam file and save output to output_subdir
-    /home/uqachoo1/mambaforge/envs/env/bin/run-trust4 -b "$TMPDIR/$(basename "$bamfile")" -f "$reference_file" -t 72 --od "$output_subdir" --abnormalUnmapFlag
+    /scratch/user/uqachoo1/miniforge3/bin/run-trust4 -b "$TMPDIR/$(basename "$bamfile")" -f "$reference_file" -t 72 --od "$output_subdir" --abnormalUnmapFlag
 done
